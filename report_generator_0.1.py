39ca402279dee6d04e7dda34b5a34779672b0b89
Tom <thomas.dell@outlook.com.au>
2022-09-30 09:36:43 +1000

    changed wording to descriptors

--- report_generator_0.1.py
+++ report_generator_0.1.py
@@ -31,38 +31,38 @@ center_frame_canvas.config(yscrollcommand=v_bar.set)
 '''
 
 
-def generate_content():
+def generate_descriptors():
     # open the file of text
-    with open('content.txt', 'r') as content:
+    with open('descriptors.txt', 'r') as descriptors:
         # read lines in the text file
-        text_contents = content.readlines()
+        descriptors_contents = descriptors.readlines()
         # grab the value in the drop down menu, which is being held by drop_down_holder
         chosen_gender = drop_down_holder.get()
 
         # 3 if statements which will swap the name and pronouns based on gender picked
         if chosen_gender == "Male (he/his)":
-            for line in text_contents:
+            for line in descriptors_contents:
                 # swap the name, and pronouns
-                male_text = line.replace("$name", str(name_entry.get())).replace("$pronoun", "he").replace("$possessive_pronoun", "his")
+                male_descriptors = line.replace("$name", str(name_entry.get())).replace("$pronoun", "he").replace("$possessive_pronoun", "his")
                 # append each line to the list
-                text_lst.append(male_text)
+                descriptors_lst.append(male_descriptors)
         make_checkboxes()
 
         if chosen_gender == "Female (she/hers)":
-            for line in text_contents:
-                female_text = line.replace("$name", str(name_entry.get())).replace("$pronoun", "she").replace("$possessive_pronoun", "hers")
-                text_lst.append(female_text)
+            for line in descriptors_contents:
+                female_descriptors = line.replace("$name", str(name_entry.get())).replace("$pronoun", "she").replace("$possessive_pronoun", "hers")
+                descriptors_lst.append(female_descriptors)
         make_checkboxes()
 
         if chosen_gender == "Genderless (they/theirs)":
-            for line in text_contents:
-                genderless_text = line.replace("$name", str(name_entry.get())).replace("$pronoun", "they").replace("$possessive_pronoun", "theirs")
-                text_lst.append(genderless_text)
+            for line in descriptors_contents:
+                genderless_descriptors = line.replace("$name", str(name_entry.get())).replace("$pronoun", "they").replace("$possessive_pronoun", "theirs")
+                descriptors_lst.append(genderless_descriptors)
         make_checkboxes()
 
 def make_checkboxes():
     i = 2
-    for index, item in enumerate(text_lst):
+    for index, item in enumerate(descriptors_lst):
         var_lst.append(tk.IntVar(value=0))
         checkbox = tk.Checkbutton(center_frame_canvas, variable=var_lst[index], text=item, wraplength=500)
         checkbox.grid(row=i, column=0, pady=2, padx=2, columnspan=4, sticky= 'w')
@@ -73,7 +73,7 @@ def clear_checkboxes():
     for checkbox in checkbox_lst:
         checkbox.destroy()
     checkbox_lst.clear()
-    text_lst.clear()
+    descriptors_lst.clear()
     var_lst.clear()
 
 def write_to_file():
@@ -84,9 +84,7 @@ def write_to_file():
             # if int_var is true
             if int_var.get():
                 # then write the the corresponding item in the text_lst list to the txt file
-                report.writelines(text_lst[x])
-    # do I need this?
-    report.close()
+                report.writelines(descriptors_lst[x])
     submit_popup()
 
 def submit_popup():
@@ -110,19 +108,42 @@ def help_popup():
     positionRight = int(popup.winfo_screenwidth()/2 - windowWidth/2)
     positionDown = int(popup.winfo_screenheight()/3 - windowHeight/2)
     popup.geometry("+{}+{}".format(positionRight, positionDown))
-    heading = tk.Label(popup, text="Templates")
+    heading = tk.Label(popup, text="Descriptors")
     heading.grid(row=0, pady=5, padx=5)
-    message = tk.Label(popup, text="To create the templates, make a text file called content, and enter your templates." + "\n" + "Make sure each one is on a new line." + "\n" + "For available substitutions, see below.")
+    message = tk.Label(popup, text="To create the descriptors, click the button below to create a text file alongside this program with some sample descriptors." + "\n" + "When writing your own, ensure each one is on a new line." + "\n" + "For available substitutions you can use in your descriptors, see below.")
     message.grid(row=0, pady=10, padx=10)
+    create_sample_button = tk.Button(popup, text="Create sample descriptors file", command = create_sample)
+    create_sample_button.grid(row=1, pady=10, padx=10)
     subs = tk.Label(popup, text="$name -> Whatever is entered into the name text field" + "\n" + "$pronoun -> The pronoun listed in the dropdown box (he/she/they)"  + "\n" + "$possessive_pronoun -> The possessive pronoun listed in the dropdown box (his/hers/theirs)")
-    subs.grid(row=1, pady=10)
-    example = tk.Label(popup, text="For example, if you had the template:" + "\n" + "$name plays well with $possessive_noun toys and $pronoun likes to play with $possessive_pronoun friends." + "\n" + "and you entered the name Miles, and the gender as he/him, it would become:" + "\n" + "Miles plays well with his toys and he likes to play with his friends.")
-    example.grid(row=2)
+    subs.grid(row=2, pady=10)
+    example = tk.Label(popup, text="For example, if you had the template:" + "\n" + "$name plays well with $possessive_noun toys and $pronoun likes to play with $possessive_pronoun friends." + "\n" + "If you entered the name Miles, and the gender as he/him, it would become:" + "\n" + "Miles plays well with his toys and he likes to play with his friends.")
+    example.grid(row=3)
     okay_button = tk.Button(popup, text="Okay!", command = popup.destroy)
-    okay_button.grid(row=3, pady=10, padx=10)
+    okay_button.grid(row=4, pady=10, padx=10)
+
+def create_sample():
+    with open("sample_descriptors.txt", "w+") as sample_descriptors:
+        sample_descriptors.write("$name is kind, creative and imaginative member of our classroom. \n")
+        sample_descriptors.write("$name seeks humour and joy in every day. \n")
+        sample_descriptors.write("$name is observant and in tune with the needs of $possessive_pronoun friends and people around them and responds with a caring nature. \n")
+    sample_create_popup()
+
+def sample_create_popup():
+    popup = tk.Tk()
+    popup.wm_title("Sample file created")
+    windowWidth = popup.winfo_reqwidth()
+    windowHeight = popup.winfo_reqheight()
+    positionRight = int(popup.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(popup.winfo_screenheight()/3 - windowHeight/2)
+    popup.geometry("+{}+{}".format(positionRight, positionDown))
+    heading = tk.Label(popup, text="Sample descriptors file created, don't forget to remove the word sample_ from the file name!")
+    heading.grid(row=0, pady=5, padx=5)
+    okay_button = tk.Button(popup, text="Okay!", command = popup.destroy)
+    okay_button.grid(row=4, pady=10, padx=10)
+
 
 # a list containing the name+pronoun swapped strings
-text_lst = []
+descriptors_lst = []
 # a number assigned to each of the lines in the text file
 var_lst = []
 # a list of all the checkboxes
@@ -158,16 +179,18 @@ spacer2 = tk.Label(top_frame, text="")
 spacer2.grid(row=0, column=2, pady=2, padx=10, rowspan=2)
 
 # the submit button, which runs the descriptions function
-button = tk.Button(top_frame, text="Generate", command=generate_content)
+button = tk.Button(top_frame, text="Generate", command=generate_descriptors)
 button.grid(row=0, column=3, pady=2, padx=2, columnspan=2)
 
 # the clear button, which runs the clear_checkboxes function
 button = tk.Button(top_frame, text="Clear", command=clear_checkboxes)
 button.grid(row=1, column=3, pady=2, padx=2, columnspan=2)
 
+# the save/export button, this will write the checked descriptors to a text file
 button = tk.Button(bottom_frame, text="Save/Export", command=write_to_file)
 button.grid(row=0, column=0)
 
+# this button opens the help section
 button = tk.Button(bottom_frame, text="Help", command=help_popup)
 button.grid(row=0, column=1)
 
